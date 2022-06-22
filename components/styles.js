09b322c96fd6edb739b5def2cfcefa714a96c05c
Thanarat Pongjan <siratat59@gmail.com>
2022-06-22 09:56:20 +0700

    new commit

---
+++ components/styles.js
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native'
+
+const styles = StyleSheet.create({
+    container:{flex:1,
+      justifyContent:'center',
+      alignContent:'center'
+    },
+    title:{
+      fontSize:20
+    },
+    warning: {
+      color:'red'
+    }
+  });
+
+export default styles;
