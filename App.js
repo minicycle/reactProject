c0d0ec2c2db6e3266e3688940f1a2f8e9366f51c
Thanarat Pongjan <siratat59@gmail.com>
2022-06-29 11:02:02 +0700

    commit

--- App.js
+++ App.js
@@ -1,30 +1,21 @@
-import { StyleSheet, Text, View ,Button} from 'react-native'
+import { StyleSheet, Text, View,Button } from 'react-native'
 import React from 'react'
+import User from './components/User'
+import Logo from './components/Logo'
 
 const App = () => {
-
-  const showData = () => {
-    alert("Hello")
-  }
   return (
-    <View style={styles.container}>
-      <Text >Button Example</Text>
-      <Button 
-      color='purple'
-      onPress={showData}
+    <View>
+      <Logo/>
+      <Button
       title='Click Me'
       />
+      <User/>
+      
     </View>
   )
 }
 
 export default App
 
-const styles = StyleSheet.create({
-  container: {
-     flex: 1,
-     alignItems: 'center',
-     justifyContent: 'center'
-     }
-    
-})
+const styles = StyleSheet.create({})
---
+++ App_HandlingEvents.js
@@ -0,0 +1,30 @@
+import { StyleSheet, Text, View ,Button} from 'react-native'
+import React from 'react'
+
+const App = () => {
+
+  const showData = () => {
+    alert("Hello")
+  }
+  return (
+    <View style={styles.container}>
+      <Text >Button Example</Text>
+      <Button 
+      color='purple'
+      onPress={showData}
+      title='Click Me'
+      />
+    </View>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({
+  container: {
+     flex: 1,
+     alignItems: 'center',
+     justifyContent: 'center'
+     }
+    
+})
