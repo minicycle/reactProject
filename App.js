7eb9a22408defa039792377805ea1e6744c1a60a
Thanarat Pongjan <siratat59@gmail.com>
2022-07-27 09:22:39 +0700

    Commit 7/27/2022

--- App.js
+++ App.js
@@ -12,6 +12,7 @@ import AlertComponent from './components/AlertComponent'
 import ImageWithTextInput from './components/ImageWithTextInput'
 import ButtonExample from './components/ButtonExample'
 import Touchable_Example from './components/Touchable_Example'
+import TouchablePractice from './components/TouchablePractice'
 const App = () => {
   
   return (
@@ -25,7 +26,9 @@ const App = () => {
       {/* <AlertComponent/> */}
       {/* <ImageWithTextInput/> */}
       {/* <ButtonExample/> */}
-      <Touchable_Example/>
+      {/* <Touchable_Example/> */}
+      <TouchablePractice/>
+
     </View>
   )
 }
